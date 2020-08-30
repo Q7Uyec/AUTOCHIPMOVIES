2c6de3734f6613544f727b09e1de9f50e6584062
Q7Uyec <89767208+Q7Uyec@users.noreply.github.com>
2020-08-30 16:44:25 +0000

    ashok broke it

--- client/components/AllProducts.js
+++ client/components/AllProducts.js
@@ -14,3 +14,41 @@ export class AllProducts extends React.Component {
   componentDidMount() {
     this.props.getAllProducts()
   }
+
+  addToCart = product => {
+    this.props.addToCartThunk(product, this.props.user)
+  }
+
+  render() {
+    const {loading} = this.props
+
+    if (loading) return <div>Putting on Airs...</div>
+    const allProducts = this.props.allProducts
+    return (
+      <div id="main">
+        <div id="container">
+          <h1>Products</h1>
+          {allProducts.map(product => (
+            <Product
+              product={product}
+              key={product.id}
+              addToCart={this.addToCart}
+            />
+          ))}
+        </div>
+      </div>
+    )
+  }
+}
+
+const mapStateToProps = state => ({
+  allProducts: state.Products.allProducts,
+  user: state.Users.id
+})
+
+const mapDispatchToProps = dispatch => ({
+  getAllProducts: () => dispatch(getAllProducts()),
+  addToCartThunk: (product, user) => dispatch(addToCartThunk(product, user))
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(AllProducts)
