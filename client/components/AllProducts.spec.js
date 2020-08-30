d4a96975e7f94bbadc6330633c723e68867789f9
Q7Uyec <89767208+Q7Uyec@users.noreply.github.com>
2020-08-30 21:50:30 +0000

    Start making booking discount block

--- client/components/AllProducts.spec.js
+++ client/components/AllProducts.spec.js
@@ -1,4 +1,6 @@
 import {expect} from 'chai'
 import React from 'react'
 import Enzyme, {shallow} from 'enzyme'
-import Adapter from 'enzyme-adapter
+import Adapter from 'enzyme-adapter-react-16'
+import  AllProducts from './AllProducts'
+import P
