4cc2fb08d320953728010d02c35dd04e1922d9d9
Q7Uyec <89767208+Q7Uyec@users.noreply.github.com>
2020-09-05 06:16:59 +0000

    Create testfile.txt

--- client/components/AllProducts.spec.js
+++ client/components/AllProducts.spec.js
@@ -3,4 +3,7 @@ import React from 'react'
 import Enzyme, {shallow} from 'enzyme'
 import Adapter from 'enzyme-adapter-react-16'
 import  AllProducts from './AllProducts'
-import P
+import Product from './Product'
+
+const adapter = new Adapter()
+Enzyme.co
