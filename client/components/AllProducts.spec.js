c06f6a5cc0adbfedd9a636bc2df505d83838aec0
Q7Uyec <89767208+Q7Uyec@users.noreply.github.com>
2020-09-06 10:36:29 +0000

    update note

--- client/components/AllProducts.spec.js
+++ client/components/AllProducts.spec.js
@@ -20,4 +20,6 @@ const productList = [
     price: 100,
     purpose: 'Productivity and Loss of Appetite',
     imageUrl:
-      'https://lh3.googleusercontent.com/GDkUTO5OsGce_APmYDByGo9uSaEk6kFjfaJNBuBhCOemLf8-sG1Zq71aAg
+      'https://lh3.googleusercontent.com/GDkUTO5OsGce_APmYDByGo9uSaEk6kFjfaJNBuBhCOemLf8-sG1Zq71aAgNjChb3Am8',
+    molecularComposition: 'CO2',
+    stock: 
