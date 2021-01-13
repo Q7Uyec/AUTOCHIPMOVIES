997574e25a61705fbe5b3b5b2094e29327cd26ca
Q7Uyec <89767208+Q7Uyec@users.noreply.github.com>
2021-01-13 14:35:01 +0000

    Proxy

--- client/components/CartItem.js
+++ client/components/CartItem.js
@@ -4,4 +4,8 @@ import { Link } from 'react-router-dom'
 const CartItem = props => {
   const product = props.cartItem.product
   const quantity = props.cartItem.quantity
-  const cartItem = props.cartIte
+  const cartItem = props.cartItem
+
+  if (!product) {
+    props.getCartThunk(props.user)
+ 
