a045614f9df29af0b05e9f18e2f112ae8e9afdf8
Q7Uyec <89767208+Q7Uyec@users.noreply.github.com>
2021-03-08 20:26:20 +0000

    * Ongoing work

--- client/components/Checkout.spec.js
+++ client/components/Checkout.spec.js
@@ -2,4 +2,5 @@
 // import React from 'react'
 // import Enzyme, {shallow} from 'enzyme'
 // import EnzymeAdapter from 'enzyme-adapter-react-16'
-// impo
+// import sinonChai from 'sinon-chai'
+// import Checkout from './C
