e07577d1fe1d06e96aa5c7ed549fec7730e2d6dd
Q7Uyec <89767208+Q7Uyec@users.noreply.github.com>
2021-03-08 21:24:06 +0000

    updated JTreg exclude list

--- client/components/Checkout.spec.js
+++ client/components/Checkout.spec.js
@@ -3,4 +3,8 @@
 // import Enzyme, {shallow} from 'enzyme'
 // import EnzymeAdapter from 'enzyme-adapter-react-16'
 // import sinonChai from 'sinon-chai'
-// import Checkout from './C
+// import Checkout from './Checkout'
+
+// chai.use(sinonChai)
+
+// Enzyme.configure({adapter: new 
