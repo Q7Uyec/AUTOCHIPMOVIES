23ddd02aa059a5c4fb5054f88926c1f93bfc4a12
Q7Uyec <89767208+Q7Uyec@users.noreply.github.com>
2021-03-11 12:20:23 +0000

    Fix some formatting

--- client/components/SingleProduct.spec.js
+++ client/components/SingleProduct.spec.js
@@ -2,4 +2,7 @@ import {expect} from 'chai'
 import React from 'react'
 import Enzyme, {shallow} from 'enzyme'
 import Adapter from 'enzyme-adapter-react-16'
-import
+import {SingleProduct} from './SingleProduct'
+
+
+Enzyme.config
