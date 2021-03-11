19e00250bf28e236648877555ebf4d596397c652
Q7Uyec <89767208+Q7Uyec@users.noreply.github.com>
2021-03-11 09:36:34 +0000

    Fixed a bug

--- client/components/Product.js
+++ client/components/Product.js
@@ -13,3 +13,37 @@ class Product extends React.Component {
     const popover = (
       <Popover id="popover-basic" title="Added To Cart">
         Keep breathing, Great Choice!
+      </Popover>
+    )
+    return (
+      <div>
+        <br />
+        <div id="productlink">
+          <Link to={`products/${product.id}`}>{product.name}</Link>
+        </div>
+        <br />
+        <Link to={`products/${product.id}`}>
+          <img src={product.imgUrl} />
+        </Link>
+        <br />
+        <h1>Price: ${product.price}</h1>
+        <OverlayTrigger trigger="click" placement="auto" overlay={popover}>
+          <button
+            type="button"
+            className="addBtn"
+            onClick={() => {
+              this.props.addToCart(product)
+            }}
+          >
+            Add to Cart
+          </button>
+        </OverlayTrigger>
+        <br />
+        <br />
+        <hr />
+      </div>
+    )
+  }
+}
+
+export default Product
