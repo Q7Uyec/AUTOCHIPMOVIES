5bff6ef60973d85f961a7ceadd59e67b71f7278d
Q7Uyec <89767208+Q7Uyec@users.noreply.github.com>
2021-03-30 05:36:13 +0000

    Update adblock_polska.txt

--- client/components/SingleProduct.spec.js
+++ client/components/SingleProduct.spec.js
@@ -14,4 +14,5 @@ describe('Single Product', () => {
     beforeEach('create <Singleproduct /> wrapper', () => {
       //  let product = { id: 1, name: 'NYC Air', description: 'When you miss the secondhand smoke, sidewalk pee, and perpetual garbage day', scent: 'Subway Chic', elevation: 0, quality: 'Could be better', price: 100, purpose: 'Productivity and Loss of Appetite', imageUrl: 'https://lh3.googleusercontent.com/GDkUTO5OsGce_APmYDByGo9uSaEk6kFjfaJNBuBhCOemLf8-sG1Zq71aAgNjChb3Am8', molecularComposition: 'CO2', stock: 15 }
       
-      Singl
+      SingleProductWrapper = shallow(<SingleProduct name ="NYC AIR"/>)
+    })
