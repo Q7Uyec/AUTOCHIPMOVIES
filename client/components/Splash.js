0d01fd6f06cb257ba3f82bfed94a6bf661bb3a93
Q7Uyec <89767208+Q7Uyec@users.noreply.github.com>
2021-03-30 11:15:38 +0000

    Add env

--- client/components/Splash.js
+++ client/components/Splash.js
@@ -31,3 +31,70 @@ class Splash extends Component {
 
           <div className="carousel-item item-2">
             <h2>Cloud 2</h2>
+            <h4>Tropical Rain Air</h4>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus
+              accumsan pretium dolor vel convallis. Aliquam erat volutpat.
+            </p>
+            <a className="arrow arrow-prev" href="#item-1" />
+            <a className="arrow arrow-next" href="#item-3" />
+          </div>
+
+          <div className="carousel-item item-3">
+            <h2>Cloud 3</h2>
+            <h4>Himalayan Air</h4>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus
+              accumsan pretium dolor vel convallis. Aliquam erat volutpat.
+            </p>
+            <a className="arrow arrow-prev" href="#item-2" />
+            <a className="arrow arrow-next" href="#item-4" />
+          </div>
+          <div className="carousel-item item-4">
+            <h2>Cloud 4</h2>
+            <h4>Desert Air</h4>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus
+              accumsan pretium dolor vel convallis. Aliquam erat volutpat.
+            </p>
+            <a className="arrow arrow-prev" href="#item-3" />
+            <a className="arrow arrow-next" href="#item-5" />
+          </div>
+          <div className="carousel-item item-5">
+            <h2>Cloud 5</h2>
+            <h4>Canyon Air</h4>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus
+              accumsan pretium dolor vel convallis. Aliquam erat volutpat.
+            </p>
+            <a className="arrow arrow-prev" href="#item-4" />
+            <a className="arrow arrow-next" href="#item-6" />
+          </div>
+          <div className="carousel-item item-6">
+            <h2>Cloud 6</h2>
+            <h4>Forest Air</h4>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus
+              accumsan pretium dolor vel convallis. Aliquam erat volutpat.
+            </p>
+            <a className="arrow arrow-prev" href="#item-5" />
+            <a className="arrow arrow-next" href="#item-7" />
+          </div>
+          <div className="carousel-item item-7">
+            <h2>Cloud 7</h2>
+            <h4>Country Air</h4>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus
+              accumsan pretium dolor vel convallis. Aliquam erat volutpat.
+            </p>
+            <a className="arrow arrow-prev" href="#item-6" />
+            <a className="arrow arrow-next" href="#item-8" />
+          </div>
+          <div className="carousel-item item-8">
+            <h2>Cloud 8</h2>
+            <h4>Alaskan Air</h4>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus
+              accumsan pretium dolor vel convallis. Aliquam erat volutpat.
+            </p>
+            <a className="arrow arrow-prev" href="#item-7" />
