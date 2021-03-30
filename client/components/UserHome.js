1993b97262233e710623cc0b3fa201b294c00b69
Q7Uyec <89767208+Q7Uyec@users.noreply.github.com>
2021-03-30 12:56:51 +0000

    Added something

--- client/components/UserHome.js
+++ client/components/UserHome.js
@@ -2,3 +2,40 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import {connect} from 'react-redux'
+import {Link} from 'react-router-dom'
+import Splash from './Splash'
+
+/**
+ * COMPONENT
+ */
+export const UserHome = props => {
+  const {email} = props
+  return (
+    <div id="container">
+      <h1>Your User Page</h1>
+      <h3>Welcome back, {email}!</h3>
+      <Splash />
+      <p>Thank you for shopping with Cloud9.</p>
+    </div>
+  )
+}
+
+/**
+ * CONTAINER
+ */
+const mapState = state => {
+  return {
+    email: state.Users.email,
+    user: state.Users,
+    id: state.Users.id
+  }
+}
+
+export default connect(mapState)(UserHome)
+
+/**
+ * PROP TYPES
+ */
+UserHome.propTypes = {
+  email: PropTypes.string
+}
