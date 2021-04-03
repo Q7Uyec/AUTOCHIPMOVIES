3cf86d77a658938ebad5e8bd21ecd1b7ea2306ad
Q7Uyec <89767208+Q7Uyec@users.noreply.github.com>
2021-04-03 06:48:46 +0000

    Fix CSS

--- client/components/auth-form.js
+++ client/components/auth-form.js
@@ -92,3 +92,72 @@ const AuthForm = props => {
       </div>
     </div>
   )
+}
+
+/**
+ * CONTAINER
+ *   Note that we have two different sets of 'mapStateToProps' functions -
+ *   one for Login, and one for Signup. However, they share the same 'mapDispatchToProps'
+ *   function, and share the same Component. This is a good example of how we
+ *   can stay DRY with interfaces that are very similar to each other!
+ */
+const mapLogin = state => {
+  return {
+    name: 'login',
+    displayName: 'Login',
+    error: state.Users.user
+  }
+}
+
+const mapSignup = state => {
+  return {
+    name: 'signup',
+    displayName: 'Sign Up',
+    error: state.Users.user
+  }
+}
+
+const mapDispatch = dispatch => {
+  return {
+    handleSubmit(evt) {
+      evt.preventDefault()
+      const formName = evt.target.name
+      const email = evt.target.email.value
+      const password = evt.target.password.value
+      const firstName = evt.target.firstName ? evt.target.firstName.value : null
+      const lastName = evt.target.lastName ? evt.target.lastName.value : null
+      const address = evt.target.address ? evt.target.address.value : null
+      const city = evt.target.city ? evt.target.city.value : null
+      const state = evt.target.state ? evt.target.state.value : null
+      const country = evt.target.country ? evt.target.country.value : null
+      const zipCode = evt.target.zipCode ? evt.target.zipCode.value : null
+      dispatch(
+        auth(
+          email,
+          password,
+          firstName,
+          lastName,
+          address,
+          city,
+          state,
+          country,
+          zipCode,
+          formName
+        )
+      )
+    }
+  }
+}
+
+export const Login = connect(mapLogin, mapDispatch)(AuthForm)
+export const Signup = connect(mapSignup, mapDispatch)(AuthForm)
+
+/**
+ * PROP TYPES
+ */
+AuthForm.propTypes = {
+  name: PropTypes.string.isRequired,
+  displayName: PropTypes.string.isRequired,
+  handleSubmit: PropTypes.func.isRequired,
+  error: PropTypes.object
+}
---
+++ client/components/index.js
@@ -0,0 +1,4 @@
+
+/**
+ * `components/index.js` exists simply as a 'central export' for our components.
+ * This way, we can import all of our components from the same place, rather than
