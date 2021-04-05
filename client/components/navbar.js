ede5286e0dd3db2b8575024fcf136ec0e5cd58cd
Q7Uyec <89767208+Q7Uyec@users.noreply.github.com>
2021-04-05 07:20:46 +0000

    update menu

--- client/components/navbar.js
+++ client/components/navbar.js
@@ -2,4 +2,6 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
 import { Link } from 'react-router-dom'
-impor
+import { logout } from '../reducers/UserReducer'
+
+const Navbar =
