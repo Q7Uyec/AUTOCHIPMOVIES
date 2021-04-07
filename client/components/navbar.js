07500d4e784df1626fd2fc6cbc849a26eec7fb6a
Q7Uyec <89767208+Q7Uyec@users.noreply.github.com>
2021-04-07 16:23:11 +0000

    added some code

--- client/components/navbar.js
+++ client/components/navbar.js
@@ -26,4 +26,6 @@ const Navbar = ({ handleClick, isLoggedIn, userId }) => {
               {/* The navbar will show these links before you log in */}
               <Link to="/products">Products</Link> |
             <Link to="/login">Login</Link> |
-            <Link to="/signup">Sign Up</Li
+            <Link to="/signup">Sign Up</Link> |
+            <Link to="/cart">
+                <img src="h
