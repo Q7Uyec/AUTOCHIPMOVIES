6d7edd5309686bc081c824468bbbd8110f75d890
Q7Uyec <89767208+Q7Uyec@users.noreply.github.com>
2021-05-03 09:46:30 +0000

    performance improvement

--- client/index.js
+++ client/index.js
@@ -2,4 +2,6 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import {Provider} from 'react-redux'
 import {Router} from 'react-router-dom'
-import history
+import history from './history'
+import store from './store'
+import Ap
