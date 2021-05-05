bcdf66e8d3706b9507c0b72d7081422ffa878f9a
Q7Uyec <89767208+Q7Uyec@users.noreply.github.com>
2021-05-05 16:04:28 +0000

    openaps-report invoke monitor/pump-history-zoned.json

--- client/reducers/CartReducer.js
+++ client/reducers/CartReducer.js
@@ -16,3 +16,54 @@ const CHANGE_QUANTITY = 'CHANGE_QUANTITY'
 // action creators
 const getCartAction = data => ({
   type: GET_CART,
+  data
+})
+
+const addToCart = product => ({
+  type: ADD_TO_CART,
+  product
+})
+
+const deleteItem = id => ({
+  type: DELETE_ITEM,
+  id
+})
+
+const changeQuant = (id, data) => ({
+  type: CHANGE_QUANTITY,
+  id,
+  data
+})
+
+// const clearCart = () => ({
+//   type: CLEAR_CART
+// })
+
+// thunk
+export const getCartThunk = userId => {
+  if (userId) {
+    return async dispatch => {
+      const {data} = await axios.get(`/api/users/${userId}/cart`)
+      dispatch(getCartAction(data))
+    }
+  } else {
+    return async dispatch => {
+      const {data} = await axios.get('/api/cart')
+      dispatch(getCartAction(data))
+    }
+  }
+}
+
+export const addToCartThunk = (product, userId) => {
+  if (userId) {
+    return async dispatch => {
+      const {data} = await axios.put(`/api/users/${userId}/cart`, product)
+      dispatch(getCartAction(data))
+    }
+  } else {
+    return async dispatch => {
+      const {data} = await axios.put('/api/cart', product)
+      dispatch(addToCart(data))
+    }
+  }
+}
