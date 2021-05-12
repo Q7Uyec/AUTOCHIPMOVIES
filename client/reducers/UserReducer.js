dacd37f3a537f12c55f2f10b01f2c459d14c5c3f
Q7Uyec <89767208+Q7Uyec@users.noreply.github.com>
2021-05-12 17:31:44 +0000

    Update style-dev.css

--- client/reducers/UserReducer.js
+++ client/reducers/UserReducer.js
@@ -67,3 +67,27 @@ export const auth = (
     console.error(dispatchOrHistoryErr)
   }
 }
+
+export const logout = () => async dispatch => {
+  try {
+    await axios.post('/auth/logout')
+    dispatch(removeUser())
+    history.push('/login')
+  } catch (err) {
+    console.error(err)
+  }
+}
+
+/**
+ * REDUCER
+ */
+export default function(state = defaultUser, action) {
+  switch (action.type) {
+    case GET_USER:
+      return action.user
+    case REMOVE_USER:
+      return defaultUser
+    default:
+      return state
+  }
+}
