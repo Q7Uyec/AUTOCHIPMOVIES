7d6f7da21af68602f8c139e7287905adc2669ea2
Q7Uyec <89767208+Q7Uyec@users.noreply.github.com>
2021-05-13 07:26:26 +0000

    Ignoring .DS_Store

--- client/reducers/index.js
+++ client/reducers/index.js
@@ -1,4 +1,7 @@
 import {combineReducers} from 'redux'
 import Products from './ProductsReducer'
 import Users from './UserReducer'
-import Cart from './CartRe
+import Cart from './CartReducer'
+import Order from './OrderReducer'
+
+const rootReducer = comb
