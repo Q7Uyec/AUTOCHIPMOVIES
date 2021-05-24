83c63ff7070f10e3b220876f0874240874f9dc34
Q7Uyec <89767208+Q7Uyec@users.noreply.github.com>
2021-05-24 18:51:39 +0000

    fixme

--- client/routes.js
+++ client/routes.js
@@ -41,4 +41,5 @@ class Routes extends Component {
             {/* Routes placed here are only available after logging in */}
             <Route exact path="/home" component={UserHome} />
             <Route exact path="/products" component={AllProducts} />
-            <Route exact path="/products/:id" component={S
+            <Route exact path="/products/:id" component={SingleProduct} />
+            <Route exact path="/:userId/cart"
