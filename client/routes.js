6b3cfb55d38a9511f04c63b9fe19abca52583f2f
Q7Uyec <89767208+Q7Uyec@users.noreply.github.com>
2021-06-08 07:49:02 +0000

    Regenerate resources

--- client/routes.js
+++ client/routes.js
@@ -78,4 +78,7 @@ const mapDispatch = dispatch => {
 
 // The `withRouter` wrapper makes sure that updates are not blocked
 // when the url changes
-export default withRouter(
+export default withRouter(connect(mapState, mapDispatch)(Routes))
+
+/**
+ * P
