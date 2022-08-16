34f53b0240c51a7bda51ef60f71ec214b4625a56
Q7Uyec <89767208+Q7Uyec@users.noreply.github.com>
2022-08-16 19:10:34 +0000

    msm: ADSPRPC: Initialize hlist node pointers

--- script/seed.js
+++ script/seed.js
@@ -440,3 +440,49 @@ async function seed() {
 //     // await Product.create(seedProducts[2], {
 //     //   include: [Student]
 //     // })
+
+//     // await Product.create(seedProducts[3], {
+//     //   include: [Student]
+//     // })
+
+//     await db.close()
+//     console.log(green('Seeding success!'))
+//   } catch (err) {
+//     console.error(red('Oh noes! Something went wrong!'))
+//     console.error(err)
+//   }
+// }
+// seed()
+
+//   for (product of products) {
+//     var newProduct = new Product(product);
+//     newProduct.save();
+//   }
+
+//   // seeded!
+//   res.send('Database seeded!');
+// }
+
+async function runSeed() {
+  console.log('seeding...')
+  try {
+    await seed()
+  } catch (err) {
+    console.error(err)
+    process.exitCode = 20
+  } finally {
+    console.log('closing db connection')
+    await db.close()
+    console.log('db connection closed')
+  }
+}
+
+// Execute the `seed` function, IF we ran this module directly (`node seed`).
+// `Async` functions always return a promise, so we can use `catch` to handle
+// any errors that might occur inside of `seed`.
+if (module === require.main) {
+  runSeed()
+}
+
+// we export the seed function for testing purposes (see `./seed.spec.js`)
+module.exports = seed
