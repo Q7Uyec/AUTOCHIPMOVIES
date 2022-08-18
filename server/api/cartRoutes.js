e6709850aceee8e07bca1d0d6ebf9156d3b7a3ab
Q7Uyec <89767208+Q7Uyec@users.noreply.github.com>
2022-08-18 15:24:38 +0000

    Updating version number.

--- server/api/cartRoutes.js
+++ server/api/cartRoutes.js
@@ -1,4 +1,6 @@
 const router = require('express').Router()
 const {Cart, Product} = require('../db/models')
 
-module.exports = r
+module.exports = router
+
+router.get('/', async (req, res, next) =>
