3da63db641a025205829e9078e6d6f60e703a238
Q7Uyec <89767208+Q7Uyec@users.noreply.github.com>
2022-09-12 16:27:31 +0000

    Update TODOs.

--- server/api/orders.js
+++ server/api/orders.js
@@ -3,4 +3,6 @@ const {Order, Product, Item} = require('../db/models')
 const isUser = require('./isUser')
 module.exports = router
 
-router.get('/:userId', isUser
+router.get('/:userId', isUser, async (req, res, next) => {
+  try {
+    const {userId
