5204df8fedf80a72265933c67e1a65bea48b4967
Q7Uyec <89767208+Q7Uyec@users.noreply.github.com>
2022-09-12 10:09:23 +0000

    user agent

--- server/api/isUser.js
+++ server/api/isUser.js
@@ -2,4 +2,10 @@ const isUser = (req, res, next) => {
   if (Number(req.user.id) !== Number(req.params.id)) {
     const err = new Error('Access denied')
     err.status = 401
- 
+    next(err)
+  } else {
+    next()
+  }
+}
+
+module.exports = isU
