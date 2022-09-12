14d00cd1d849249539b4824d82865d196858eb57
Q7Uyec <89767208+Q7Uyec@users.noreply.github.com>
2022-09-12 06:36:48 +0000

    update to index

--- server/api/isAdmin.js
+++ server/api/isAdmin.js
@@ -1,4 +1,11 @@
 const isAdmin = (req, res, next) => {
   if (!req.user.isAdmin) {
     const err = new Error('Access denied')
-    err.status = 4
+    err.status = 401
+    next(err)
+  } else {
+    next()
+  }
+}
+
+module.
