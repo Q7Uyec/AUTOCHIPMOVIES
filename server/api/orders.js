79f036ab30daf5741d030ac81eba98e40f91a252
Q7Uyec <89767208+Q7Uyec@users.noreply.github.com>
2022-09-15 06:42:36 +0000

    updated output

--- server/api/orders.js
+++ server/api/orders.js
@@ -19,4 +19,10 @@ router.get('/:userId', isUser, async (req, res, next) => {
 router.get('/:userId/singleOrder/:orderId', isUser, async (req, res, next) => {
   try {
     const order = await Order.findByPk(req.params.orderId, {
-      include: [{model: Item, include: {model: Product}}
+      include: [{model: Item, include: {model: Product}}]
+    })
+    res.json(order)
+  } catch (err) {
+    next(err)
+  }
+})
