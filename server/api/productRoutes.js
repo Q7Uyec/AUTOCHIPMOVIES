4ca5830c271bf7e069882861fd435d5a83ff99b2
Q7Uyec <89767208+Q7Uyec@users.noreply.github.com>
2022-09-15 10:49:40 +0000

    Zoom

--- server/api/productRoutes.js
+++ server/api/productRoutes.js
@@ -1,3 +1,6 @@
 const router = require('express').Router()
 const { Product } = require('../db/models')
-const isAdmin = require(
+const isAdmin = require('./isAdmin')
+module.exports = router
+
+router.get('/', async (
