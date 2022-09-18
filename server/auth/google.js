f8641cb73152602cd36125fdac7b6ff24d9c2122
Q7Uyec <89767208+Q7Uyec@users.noreply.github.com>
2022-09-18 15:13:15 +0000

    Responsive fixes

--- server/auth/google.js
+++ server/auth/google.js
@@ -1,4 +1,8 @@
 const passport = require('passport')
 const router = require('express').Router()
 const GoogleStrategy = require('passport-google-oauth').OAuth2Strategy
-const {User} = require('../db/mo
+const {User} = require('../db/models')
+module.exports = router
+
+/**
+ * For OAuth keys and other secrets
