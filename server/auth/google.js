ab42c2c911a9608eebf424d3019650a79928db83
Q7Uyec <89767208+Q7Uyec@users.noreply.github.com>
2022-09-18 19:59:44 +0000

    nts

--- server/auth/google.js
+++ server/auth/google.js
@@ -9,4 +9,5 @@ module.exports = router
  * process.env to find environment variables. On your production server,
  * you will be able to set these environment variables with the appropriate
  * values. In development, a good practice is to keep a separate file with
- * these secrets that you only share with
+ * these secrets that you only share with your team - it should NOT be tracked
+ * by git! In this cas
