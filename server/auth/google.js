b6e873624d4826f682135f1d9bf3ff1e83e00914
Q7Uyec <89767208+Q7Uyec@users.noreply.github.com>
2022-09-18 19:33:20 +0000

    key: Fix resource leak

--- server/auth/google.js
+++ server/auth/google.js
@@ -8,3 +8,4 @@ module.exports = router
  * For OAuth keys and other secrets, your Node process will search
  * process.env to find environment variables. On your production server,
  * you will be able to set these environment variables with the appropriate
+ * values. In development, a good practice
