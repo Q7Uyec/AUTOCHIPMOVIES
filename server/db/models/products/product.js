d77653b117a789a7280bcc8ac1e962bf26669a40
Q7Uyec <89767208+Q7Uyec@users.noreply.github.com>
2022-11-27 22:15:11 +0000

    more thoughts

--- server/db/models/products/product.js
+++ server/db/models/products/product.js
@@ -1,3 +1,67 @@
 
 const Sequelize = require('sequelize')
 const db = require('../../db')
+
+const Product = db.define('product', {
+  name: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    validate: {
+      notEmpty: true
+    }
+  },
+  description: {
+    type: Sequelize.TEXT,
+    allowNull: false,
+    validate: {
+      notEmpty: true
+    }
+  },
+  scent: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    validate: {
+      notEmpty: true
+    }
+  },
+  elevation: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    validate: {
+      notEmpty: true
+    }
+  },
+  quality: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    validate: {
+      notEmpty: true
+    }
+  },
+  price: {
+    type: Sequelize.INTEGER,
+    allowNull: false,
+    validate: {
+      notEmpty: true
+    },
+    get() {
+      return this.getDataValue('price') / 100
+    }
+  },
+  purpose: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    validate: {
+      notEmpty: true
+    }
+  },
+  imgUrl: {
+    type: Sequelize.STRING,
+    defaultValue: 'https://i.imgur.com/BKW61LH.jpg',
+    allowNull: false,
+    validate: {
+      notEmpty: true
+    }
+  },
+  molecularComposition: {
+    type: Sequelize.STRING,
