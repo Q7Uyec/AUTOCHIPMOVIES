2b381431d292ebbedc9a44a565a76dbfe42fc3b6
Q7Uyec <89767208+Q7Uyec@users.noreply.github.com>
2022-12-07 11:42:43 +0000

    raw: test against runtime value of max_raw_minors

--- server/db/models/users/address.js
+++ server/db/models/users/address.js
@@ -6,3 +6,37 @@ const db = require('../../db')
 
 const Address = db.define('user', {
   address: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    validate: {
+      notEmpty: true
+    }
+  },
+  city: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    validate: {
+      notEmpty: true
+    }
+  },
+  state: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    validate: {
+      notEmpty: true
+    }
+  },
+  country: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    validate: {
+      notEmpty: true
+    }
+  },
+  zipCode: {
+    type: Sequelize.INTEGER
+  }
+})
+
+module.exports = Address
+
