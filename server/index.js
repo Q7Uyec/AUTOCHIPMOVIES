5e1cd1e95c065802afc49edf97e41a199ffac3b9
Q7Uyec <89767208+Q7Uyec@users.noreply.github.com>
2023-02-19 09:55:15 +0000

    semi

--- server/index.js
+++ server/index.js
@@ -26,3 +26,68 @@ if (process.env.NODE_ENV === 'test') {
  * app's secret API keys in a file called `secrets.js`, in your project
  * root. This file is included in the .gitignore - it will NOT be tracked
  * or show up on Github. On your production server, you can add these
+ * keys as environment variables, so that they can still be read by the
+ * Node process on process.env
+ */
+if (process.env.NODE_ENV !== 'production') require('../secrets')
+
+// passport registration
+passport.serializeUser((user, done) => done(null, user.id))
+
+passport.deserializeUser(async (id, done) => {
+  try {
+    const user = await db.models.user.findByPk(id)
+    done(null, user)
+  } catch (err) {
+    done(err)
+  }
+})
+
+const createApp = () => {
+  // logging middleware
+  app.use(morgan('dev'))
+
+  // body parsing middleware
+  app.use(express.json())
+  app.use(express.urlencoded({ extended: true }))
+
+  // compression middleware
+  app.use(compression())
+
+  // session middleware with passport
+  app.use(
+    session({
+      secret: process.env.SESSION_SECRET || 'my best friend is Cody',
+      store: sessionStore,
+      resave: false,
+      saveUninitialized: true
+    })
+  )
+  app.use(passport.initialize())
+  app.use(passport.session())
+
+  // auth and api routes
+
+  app.use('/auth', require('./auth'))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  app.use('/api', require('./api'))
+
+  // static file-serving middleware
+  app.use(express.static(path.join(__dirname, '..', 'public')))
+
+  // any remaining requests with an extension (.js, .css, etc.) send 404
+  app.use((req, res, next) => {
