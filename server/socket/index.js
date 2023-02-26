254bcc58a71161f642ec94cf4c0629643443dcaa
Q7Uyec <89767208+Q7Uyec@users.noreply.github.com>
2023-02-26 05:25:59 +0000

    Remove println

--- server/socket/index.js
+++ server/socket/index.js
@@ -2,4 +2,5 @@ module.exports = io => {
   io.on('connection', socket => {
     console.log(`A socket connection to the server has been made: ${socket.id}`)
 
-    socket.on('disconn
+    socket.on('disconnect', () => {
+      console.log(`Connection ${socket.id} has left th
